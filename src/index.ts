65a54b3507479dfbb87f34ce1bdb4e77cf185d9b
Minho Yi <tisohjung@gmail.com>
2025-03-05 13:06:08 +0900

    🎉 example of admins command

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+const { App } = require('@slack/bolt');
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+appToken: process.env.SLACK_APP_TOKEN,
+  token: process.env.SLACK_BOT_TOKEN,
+  socketMode: true,
+});
+
+app.command('/admins', async ({ ack, respond, client }) => {
+  const { members } = await client.users.list();
+  const admins = members.filter((member) => member.is_admin);
+
+  console.log(`⭐ ${JSON.stringify(admins, null, 2)}`);
+  await ack();
+  await respond({
+    blocks: [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `The admins are:\n${admins.map((admin) => `@${admin.name}`).join(", ")}`
+        }
+      },
+    ],
+    response_type: "ephemeral",
+    // response_type: "in_channel",
+  });
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000).catch((error) => {
+    console.error('An error occurred:', error);
+  });
+
+  app.logger.info('⚡️ Bolt app is running!');
+})();
